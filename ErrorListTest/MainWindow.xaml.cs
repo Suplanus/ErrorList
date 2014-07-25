e2d961086dd244da2eb08eca50c1c9517082dc8d
Johann Weiher <info@suplanus.de>
2014-07-25 08:55:47 +0200

    Demo added

---
+++ ErrorListTest/MainWindow.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System.Windows;
+
+namespace ErrorListTest
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnAddSampleData_OnClick(object sender, RoutedEventArgs e)
+        {
+            errorList.AddError("Error unable to do something \"Name: Write PHP\"");
+            errorList.AddError("Error unable to do something \"Name: Write Flash\"");
+            errorList.AddWarning("Error unable to do something \"Name: Program in F#, yet\"");
+            errorList.AddInformation("Note: I need a better hobby than wasting my lunch coding..");
+        }
+
+        private void BtnRemoveSampleData_OnClick(object sender, RoutedEventArgs e)
+        {
+            errorList.ClearAll();
+        }
+    }
+}
